4e4e7b9e98a2b38e3d1886b74cf229e7c44af7b3
Mateus Macedo <mhbassomacedo@gmail.com>
2023-12-03 00:24:00 -0300

    Adding Best Sellers section
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- lib/mobile.dart
+++ lib/mobile.dart
@@ -11,95 +11,307 @@ class MobileContent extends StatelessWidget {
           title: const Text('Tela de testes mobile do Figma'),
         ),
         backgroundColor: Colors.grey[300],
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                      onPressed: () {
-                        // Lógica do primeiro ícone
-                      },
-                      icon: Icon(Icons.home),
-                    ),
+        body: SingleChildScrollView(
+          child: Container(
+            height: 3548,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () {
+                            // Lógica do primeiro ícone
+                          },
+                          icon: Icon(Icons.home),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () {
+                            // Lógica do segundo ícone
+                          },
+                          icon: Icon(Icons.search),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          color: Colors.amberAccent,
+                          height: 48,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () {
+                            // Lógica do quarto ícone
+                          },
+                          icon: Icon(Icons.person),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () {
+                            // Lógica do quinto ícone
+                          },
+                          icon: Icon(Icons.settings),
+                        ),
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                      onPressed: () {
-                        // Lógica do segundo ícone
-                      },
-                      icon: Icon(Icons.search),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Expanded(
                     child: Container(
-                      color: Colors.amberAccent,
-                      height: 48,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                      onPressed: () {
-                        // Lógica do quarto ícone
-                      },
-                      icon: Icon(Icons.person),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                      onPressed: () {
-                        // Lógica do quinto ícone
-                      },
-                      icon: Icon(Icons.settings),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                height: 551,
-                color: Colors.red,
-                child: Row(children: [
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(left: 20, bottom: 20),
-                          child: Text("Texto 1", textAlign: TextAlign.left),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(left: 20, bottom: 20),
-                          child: Text("Texto 2", textAlign: TextAlign.left),
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(left: 20, bottom: 20),
-                            child: Expanded(
-                              child: Container(
-                                height: 40,
-                                width: 68,
-                                color: Colors.blueAccent,
+                      height: 551,
+                      color: Colors.red,
+                      child: Row(children: [
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(left: 20, bottom: 20),
+                                child:
+                                    Text("Texto 1", textAlign: TextAlign.left),
                               ),
-                            )),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 20, bottom: 20),
+                                child:
+                                    Text("Texto 2", textAlign: TextAlign.left),
+                              ),
+                              Padding(
+                                  padding:
+                                      EdgeInsets.only(left: 20, bottom: 20),
+                                  child: Expanded(
+                                    child: Container(
+                                      height: 40,
+                                      width: 68,
+                                      color: Colors.blueAccent,
+                                      child: Placeholder(),
+                                    ),
+                                  )),
+                            ]),
                       ]),
-                ]),
-              ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Best Sellers",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 335,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.red[400],
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Container(
+                                            height: 20,
+                                            width: 20,
+                                            color: Colors.green[900],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text("Tailored Strectch"),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text("Turn it up pants"),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  EdgeInsets.only(right: 20),
+                                              child: Text(
+                                                "\$180",
+                                                textAlign: TextAlign.end,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Container(
+                                                width: 20,
+                                                height: 20,
+                                                color: Colors.amber[100],
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Container(
+                                                width: 20,
+                                                height: 20,
+                                                color: Colors.amber[200],
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Container(
+                                                width: 20,
+                                                height: 20,
+                                                color: Colors.amber[300],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 20,),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.red[900],
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Container(
+                                            height: 20,
+                                            width: 20,
+                                            color: Colors.green[900],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text("Tailored Silk"),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text("Make A splash"),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  EdgeInsets.only(right: 20),
+                                              child: Text(
+                                                "\$120",
+                                                textAlign: TextAlign.end,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Container(
+                                                width: 20,
+                                                height: 20,
+                                                color: Colors.amber[100],
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Container(
+                                                width: 20,
+                                                height: 20,
+                                                color: Colors.amber[200],
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Container(
+                                                width: 20,
+                                                height: 20,
+                                                color: Colors.amber[300],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 1551,
+                    color: Colors.yellow,
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Container(
-                height: 551,
-                color: Colors.yellow,
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
